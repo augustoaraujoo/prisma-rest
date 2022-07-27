6d98fb62b459087929e19d3ece79e369573548a6
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-07-26 21:50:41 -0300

    build js

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { app } from '.';
 import "express-async-errors";
+const port = process.env.PORT || 1010;
 
-app.listen(1010, () => {
+app.listen(port, () => {
     console.log("rodando");
 })
